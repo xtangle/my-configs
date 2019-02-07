f1749814351b6e6dec3065649a81a8a849b3f1d3
Jacky Xu <jackyxu07@gmail.com>
2019-02-07 08:49:24 -0500

    fix: shellcheck lint errors

--- scripts/export/export-sublime.sh
+++ scripts/export/export-sublime.sh
@@ -7,4 +7,7 @@ cp -f "${sublime_configs}/Preferences.sublime-settings" "${CONFIG_FILES}/Sublime
 cp -f "${sublime_configs}/Default (Linux).sublime-keymap" "${CONFIG_FILES}/Sublime"
 
 # don't include dpi scale in exported configs, it is configured automatically in import-sublime.sh
-cat <<< "$(cat "${sublime_configs}/Preferences.sublime-settings" | sed '/\/\//d' | jq 'del(.dpi_scale)')" > "${sublime_configs}/Preferences.sublime-settings"
+settings="${CONFIG_FILES}/Sublime/Preferences.sublime-settings"
+tmp_settings="${settings}.tmp"
+sed '/\/\//d' "${settings}" | jq 'del(.dpi_scale)' > "${tmp_settings}" \
+  && mv "${tmp_settings}" "${settings}"
