2d590a835dd3b3569988f881cadb87c95e627fff
Jacky Xu <jackyxu07@gmail.com>
2019-03-02 09:20:03 -0500

    chore: update yarn and IntelliJ settings

--- scripts/configure/configure-yarn.sh
+++ scripts/configure/configure-yarn.sh
@@ -6,6 +6,6 @@ set -e
 load "${HOME}/.nvm/nvm.sh"
 
 # installs useful global packages for yarn
-packages=(@angular/cli @vue/cli create-react-app polymer-cli yo degit how-2)
+packages=(@angular/cli @vue/cli create-react-app polymer-cli serve yo degit how-2)
 yarn global --silent add "${packages[@]}" 2>/dev/null
 yarn global --silent upgrade --latest 2>/dev/null
