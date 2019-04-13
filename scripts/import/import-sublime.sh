24ad8fca84701b075c3ed5167e5fa5c4162c4796
Jacky Xu <jackyxu07@gmail.com>
2019-04-12 19:44:18 -0500

    fix: scaling for sublime

--- scripts/import/import-sublime.sh
+++ scripts/import/import-sublime.sh
@@ -14,6 +14,6 @@ if [[ ${SCREEN_WIDTH} -gt 2560 ]]; then
   scale_factor=$(bc <<< "scale=2; 1 + 3 * ((${SCREEN_WIDTH} / 2560) - 1)")
   settings="${sublime_configs}/Preferences.sublime-settings"
   tmp_settings="${settings}.tmp"
-  sed '/\/\//d' "${settings}" | jq ". + {dpi_scale: ${scale_factor}}" > "${tmp_settings}" \
+  sed '/\/\//d' "${settings}" | jq ". + {ui_scale: ${scale_factor}}" > "${tmp_settings}" \
     && mv "${tmp_settings}" "${settings}"
 fi
