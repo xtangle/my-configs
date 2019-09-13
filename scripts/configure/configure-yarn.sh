59822a6bdf1d371b2372676808076347c8b0d999
Jacky Xu <jackyxu07@gmail.com>
2019-09-13 18:31:37 -0400

    Increase verbosity when configuring yarn

--- scripts/configure/configure-yarn.sh
+++ scripts/configure/configure-yarn.sh
@@ -7,5 +7,5 @@ load "${HOME}/.nvm/nvm.sh"
 
 # installs useful global packages for yarn
 packages=(@angular/cli @vue/cli create-react-app polymer-cli serve yo degit how-2)
-yarn global --silent add "${packages[@]}" 2>/dev/null
-yarn global --silent upgrade --latest 2>/dev/null
+yarn global add "${packages[@]}" 2>/dev/null
+yarn global upgrade --latest 2>/dev/null
