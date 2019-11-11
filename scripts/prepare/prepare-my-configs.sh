c8f03ffacadbfa5643f93cf570f15077f96985bb
Jacky Xu <jackyxu07@gmail.com>
2019-11-10 23:27:41 -0500

    refactor

--- scripts/prepare/prepare-my-configs.sh
+++ scripts/prepare/prepare-my-configs.sh
@@ -4,10 +4,8 @@ set -e
 
 # bootstraps the my-configs bootstrap script to be available in the path
 bin_dir="\${HOME}/Projects/my-configs/scripts/bin"
-import_dir="\${HOME}/Projects/my-configs/scripts/import"
-export_dir="\${HOME}/Projects/my-configs/scripts/export"
 mkdir -p "${HOME}/.rc"
 cat > "${HOME}/.rc/my-configs" << EOL
-export PATH="${bin_dir}:${import_dir}:${export_dir}:\${PATH}"
+export PATH="${bin_dir}:\${PATH}"
 EOL
 source_in_profile "\${HOME}/.rc/my-configs"
